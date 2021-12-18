53f0f2e8d2ad503c9a46821537025de2b5e765b9
francohandel <francohandel@gmail.com>
2021-12-18 09:43:57 +0800

    web push

--- scripts/script.js
+++ scripts/script.js
@@ -42,3 +42,50 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+function subscribe() {
+  navigator.serviceWorker.ready
+    .then(function(registration) {
+      const vapidPublicKey = "BIN2Jc5Vmkmy-S3AUrcMlpKxJpLeVRAfu9WBqUbJ70SJOCWGCGXKY-Xzyh7HDr6KbRDGYHjqZ06OcS3BjD7uAm8";
+
+      return registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+      });
+    })
+    .then(function(subscription) {
+      console.log(
+        JSON.stringify({
+          subscription: subscription,
+        })
+      );
+    })
+    .catch(err => console.error(err));
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)));
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+  navigator.serviceWorker.ready
+    .then(function(registration) {
+      return registration.pushManager.getSubscription();
+    })
+    .then(function(subscription) {
+      if (!subscription) {
+        subscribe();
+      } else {
+        console.log(
+          JSON.stringify({
+            subscription: subscription,
+          })
+        );
+      }
+    });
+}
